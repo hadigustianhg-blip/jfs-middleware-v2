1485fcf56771a662f5439a9519147624ae312529
Hadi Gustian <hadi.gustian.hg@gmail.com>
2026-08-28 02:33:01 +0700

    fix: fallback AUTH_TOKEN to process.env.JFS_AUTH_TOKEN in server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors());
 app.use(express.json());
 
 // 🔐 TOKEN
-let AUTH_TOKEN = process.env.AUTH_TOKEN || "";
+let AUTH_TOKEN = process.env.AUTH_TOKEN || process.env.JFS_AUTH_TOKEN || "";
 const authManager = createJfsAuthManager({
   initialToken: AUTH_TOKEN,
   onToken: token => {
